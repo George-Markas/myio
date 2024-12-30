c2ca5620a23cb6fa7fcc6c34e5ba5715a7006c4b
George <georgmarkanton@proton.me>
2024-12-30 19:20:34 +0200

    Initial upload

---
+++ main.c
@@ -0,0 +1,18 @@
+// myio demo
+#define MYIO_IMPLEMENTATION
+#include "myio.h"
+
+int main(void) {
+    int an_int;
+    float a_float;
+
+    puts("Input an integer:");
+    read_int(&an_int, 1, 1);
+
+    puts("\nInput a float:");
+    read_float(&a_float, 1, 1);
+
+    printf("\nHere's your int \033[0;32m%d\033[0m and here is your damn float \033[0;32m%.2f\033[0m.\n", an_int, a_float);
+
+    return EXIT_SUCCESS;
+}
---
+++ myio.h
@@ -0,0 +1,125 @@
+#ifndef MYIO_H
+#define MYIO_H
+
+int input_sanitizer(const char *buffer, int allow_negative, int allow_float);
+int read_int(int *var, ...);
+int read_float(float *var, ...);
+
+#endif // MYIO_H
+
+#ifdef MYIO_IMPLEMENTATION
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * @brief Provides checks for non-numerics and negatives if validity_check_enabled is true.
+ */
+inline int input_sanitizer(const char *buffer, int allow_negative, int allow_float) {
+    // In case of empty input, even if NULL macro is not defined
+    if(buffer == NULL || *buffer == '\0')
+        return 0;
+
+    // Handle sign
+    int i = 0;
+    if(buffer[0] == '-') {
+        if(allow_negative) {
+            i++;
+        } else {
+            return 0;
+        }
+    }
+
+    // In case input is just a newline
+    if(buffer[i] == '\n')
+        return 0;
+
+    for(; buffer[i] != '\n'; i++) {
+        if(allow_float) {
+            #define FLOAT_CHECK && (buffer[i] != 0x2e) // 0x2e = '.'
+        }
+        else
+            #define FLOAT_CHECK
+
+        if(!isdigit(buffer[i]) FLOAT_CHECK)
+            return 0;
+    }
+
+    return 1;
+}
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#define BUFFER_SIZE 256
+
+/**
+ * @brief Reads an integer from stdin with optional checks for non-numerics and/or negatives.
+ *
+ * it read_int(int* var, int validity_check_enabled, int allow_negative)
+ */
+inline int read_int(int* var, ...) {
+    va_list args;
+    va_start(args, var);
+    char buffer[BUFFER_SIZE];
+
+    // If read fails, stop
+    if(!fgets(buffer, BUFFER_SIZE, stdin))
+        return EXIT_FAILURE;
+
+    int validity_check_enabled = va_arg(args, int);
+    if(validity_check_enabled) {
+        int allow_negative = va_arg(args, int);
+        char *str = "n";
+
+        if (!allow_negative)
+            str = " non-negative";
+
+        va_end(args);
+
+        while (!input_sanitizer(buffer, allow_negative, 0)) {
+            printf("\n\033[3;31mMust be a%s integer! Retry...\033[0m\n", str);
+            fgets(buffer, BUFFER_SIZE, stdin);
+        }
+    }
+
+    const int val = (int) strtol(buffer, NULL, 10);
+    *var = val;
+    return EXIT_SUCCESS;
+}
+
+/**
+ * @brief Reads a float from stdin with optional checks for non-numerics and/or negatives.
+ *
+ * int read_float(float* var, int validity_check_enabled, int allow_negative)
+ */
+inline int read_float(float* var, ...) {
+    va_list args;
+    va_start(args, var);
+    char buffer[BUFFER_SIZE];
+
+    // If read fails, stop
+    if(!fgets(buffer, BUFFER_SIZE, stdin))
+        return EXIT_FAILURE;
+
+    int validity_check_enabled = va_arg(args, int);
+    if(validity_check_enabled) {
+        int allow_negative = va_arg(args, int);
+        char *str = " f";
+
+        if (!allow_negative)
+            str = " non-negative f";
+
+        va_end(args);
+
+        while (!input_sanitizer(buffer, allow_negative, 1)) {
+            printf("\n\033[3;31mMust be a%sloat! Retry...\033[0m\n", str);
+            fgets(buffer, BUFFER_SIZE, stdin);
+        }
+    }
+
+    const float val = strtof(buffer, NULL);
+    *var = val;
+    return EXIT_SUCCESS;
+}
+
+#endif // MYIO_IMPLEMENTATION
